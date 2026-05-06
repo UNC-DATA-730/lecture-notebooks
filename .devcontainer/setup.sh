e5abdd1f6a0199a74683d795bbab0579d71371c7
Chuck Pepe-Ranney <chuck.peperanney@gmail.com>
2026-05-06 19:31:06 -0400

    devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# mamba is much faster than conda for solving r-base + tidymodels
+conda install -n base -c conda-forge -y mamba
+mamba env update -n base -f environment.yml
+
+# Register both kernels with Jupyter
+mamba run -n base python -m ipykernel install --user --name python3 --display-name "Python 3"
+mamba run -n base R -e 'IRkernel::installspec(user = TRUE, displayname = "R")'
